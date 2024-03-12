2a12cfdaeb70fd7802157332bfc0fd7c5bd4c6ef
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-12 09:16:27 +0530

    http module imported

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ValidationMessagesComponent } from './components/errors/validation-messages/validation-messages.component';
 import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ValidationMessagesComponent, NotFoundComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   exports: [ReactiveFormsModule],
 })
 export class SharedModule {}

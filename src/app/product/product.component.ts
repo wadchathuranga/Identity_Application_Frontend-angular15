2a1dc5aaff8f0221a1801235ae0bffccbff4a8f4
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-13 22:59:22 +0530

    prod updated

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  //
+  message: string | undefined;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (response: any) => {
+        this.message = response.value.message;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }

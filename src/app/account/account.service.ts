dc433ff4f1c541c17242d9ae9e106dd90d69ea5b
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-13 22:59:41 +0530

    acc service updated

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -1,18 +1,82 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserRegisterModel } from '../shared/models/UserRegisterModel';
 import { environment } from 'src/environments/environment.development';
+import { UserLoginModel } from '../shared/models/UserLoginModel';
+import { UserModel } from '../shared/models/UserModel';
+import { ReplaySubject, map, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  constructor(private http: HttpClient) {}
+  private userSource = new ReplaySubject<UserModel | null>(1);
+  user$ = this.userSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  refreshUser(jwt: string | null) {
+    if (jwt === null) {
+      this.userSource.next(null);
+      return of(undefined);
+    }
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'Bearer ' + jwt);
+
+    return this.http
+      .get<UserModel>(`${environment.appUrl}/account/refresh-user-token`, {
+        headers,
+      })
+      .pipe(
+        map((userModel: UserModel) => {
+          if (userModel) {
+            this.setUser(userModel);
+          }
+        })
+      );
+  }
 
   userRegister(userRegisterModel: UserRegisterModel) {
     return this.http.post(
       `${environment.appUrl}/account/register`,
       userRegisterModel
     );
+  }
+
+  userLogin(userLoginModel: UserLoginModel) {
+    return this.http
+      .post<UserModel>(`${environment.appUrl}/account/login`, userLoginModel)
+      .pipe(
+        map((userModel: UserModel) => {
+          if (userModel) {
+            this.setUser(userModel);
+            // return userModel; // we dont need data to other side
+          }
+          // return null; // we dont need data to other side
+        })
+      );
+  }
+
+  userLogout() {
+    localStorage.removeItem(environment.userKey);
+    this.userSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  getJWT() {
+    const userData = localStorage.getItem(environment.userKey);
+    if (userData) {
+      const userInfo: UserModel = JSON.parse(userData);
+      return userInfo.jwt;
+    } else {
+      return null;
+    }
+  }
+
+  private setUser(userModel: UserModel) {
+    localStorage.setItem(environment.userKey, JSON.stringify(userModel));
+    this.userSource.next(userModel);
   }
 }

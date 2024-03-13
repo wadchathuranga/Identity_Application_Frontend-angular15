d225558e98bb1f6fc977ebe57789585951905371
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-13 23:01:52 +0530

    loging updated

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { SharedService } from 'src/app/shared/shared.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserModel } from 'src/app/shared/models/UserModel';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  //
+  loginForm: FormGroup = new FormGroup({});
+  submitted = false;
+  errorMessages: string[] = [];
+  returnUrl: string | null = null;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private accountService: AccountService,
+    private sharedService: SharedService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.accountService.user$.pipe(take(1)).subscribe({
+      next: (user: UserModel | null) => {
+        if (user) {
+          this.router.navigateByUrl('/');
+        } else {
+          this.activatedRoute.queryParamMap.subscribe({
+            next: (params: any) => {
+              if (params) {
+                this.returnUrl = params.get('returnUrl');
+              }
+            },
+          });
+        }
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  login() {
+    this.submitted = true;
+    this.errorMessages = [];
+
+    if (this.loginForm.valid) {
+      this.accountService.userLogin(this.loginForm.value).subscribe({
+        next: (response: any) => {
+          if (this.returnUrl) {
+            this.router.navigateByUrl(this.returnUrl);
+          } else {
+            this.router.navigateByUrl('/');
+          }
+        },
+        error: (error) => {
+          if (error.error.errors) {
+            this.errorMessages = error.error.errors;
+          } else {
+            this.errorMessages.push(error.error);
+          }
+        },
+      });
+    }
+  }
 }

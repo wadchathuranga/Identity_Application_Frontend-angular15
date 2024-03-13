e2c6aea168040e925741c9106d0382079f4ba068
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-13 22:59:04 +0530

    navbar updated

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  //
+  constructor(public accountService: AccountService) {}
 
+  userLogout() {
+    this.accountService.userLogout();
+  }
 }

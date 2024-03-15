46a7fd79390d20c615e7d5de01f0479ce9490153
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-15 12:03:57 +0530

    form initialized

--- src/app/account/reset-password/reset-password.component.ts
+++ src/app/account/reset-password/reset-password.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SharedService } from 'src/app/shared/shared.service';
+import { take } from 'rxjs';
+import { UserModel } from 'src/app/shared/models/account/UserModel';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  styleUrls: ['./reset-password.component.css'],
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent implements OnInit {
+  //
+  resetPasswordForm: FormGroup = new FormGroup({});
+  token: string | undefined;
+  email: string | undefined;
+  submitted = false;
+  errorMessages: string[] = [];
 
+  constructor(
+    private accountService: AccountService,
+    private sharedService: SharedService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.accountService.user$.pipe(take(1)).subscribe({
+      next: (user: UserModel | null) => {
+        if (user) {
+          this.router.navigateByUrl('/');
+        } else {
+          this.activatedRoute.queryParamMap.subscribe({
+            next: (params: any) => {
+              this.token = params.get('token');
+              this.email = params.get('email');
+
+              if (this.token && this.email) {
+                this.initializeForm(this.email);
+              } else {
+                this.router.navigateByUrl('/account/login');
+              }
+            },
+          });
+        }
+      },
+    });
+  }
+
+  initializeForm(username: string) {
+    this.resetPasswordForm = this.formBuilder.group({
+      email: [{ value: username, dissabled: true }],
+      newPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(15),
+        ],
+      ],
+    });
+  }
 }

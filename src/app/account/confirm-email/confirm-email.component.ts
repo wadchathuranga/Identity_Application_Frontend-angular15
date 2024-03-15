78fdb33c1e58ee70e3d83cc4077e94e1390ec656
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-03-15 10:44:25 +0530

    confirm email implemented

--- src/app/account/confirm-email/confirm-email.component.ts
+++ src/app/account/confirm-email/confirm-email.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+import { SharedService } from 'src/app/shared/shared.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
+import { ConfirmEmail } from 'src/app/shared/models/account/ConfirmEmail';
+import { UserModel } from 'src/app/shared/models/account/UserModel';
 
 @Component({
   selector: 'app-confirm-email',
   templateUrl: './confirm-email.component.html',
-  styleUrls: ['./confirm-email.component.css']
+  styleUrls: ['./confirm-email.component.css'],
 })
-export class ConfirmEmailComponent {
+export class ConfirmEmailComponent implements OnInit {
+  //
+  success = false;
 
+  constructor(
+    private accountService: AccountService,
+    private sharedService: SharedService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.accountService.user$.pipe(take(1)).subscribe({
+      next: (user: UserModel | null) => {
+        if (user) {
+          this.router.navigateByUrl('/');
+        } else {
+          this.activatedRoute.queryParamMap.subscribe({
+            next: (params: any) => {
+              const confirmEmail: ConfirmEmail = {
+                token: params.get('token'),
+                email: params.get('email'),
+              };
+
+              this.accountService.confirmEmail(confirmEmail).subscribe({
+                next: (response: any) => {
+                  this.success = true;
+                  this.sharedService.showNotification(
+                    true,
+                    response.value.title,
+                    response.value.message
+                  );
+                },
+                error: (error) => {
+                  this.success = false;
+                  this.sharedService.showNotification(
+                    false,
+                    'Failed',
+                    error.error
+                  );
+                },
+              });
+            },
+          });
+        }
+      },
+    });
+  }
+
+  resendEmailConfirmtionLink() {
+    this.router.navigateByUrl(
+      '/account/send-email/resend-email-confirmation-link'
+    );
+  }
 }
